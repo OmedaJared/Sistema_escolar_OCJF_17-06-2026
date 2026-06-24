429af5c908e30bdcbd58727f80a75f5bfb046f1d
OmedaJared <24308060610036@cetis61.edu.mx>
2026-06-24 11:32:24 -0500

    AAAa

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Configuración de MongoDB (Local o Atlas a través de variables de entorno)
 MONGODB_USERNAME="j25759298_db_user"
 MONGODB_PASSWORD="y6Kf7b33aj3jaadx"
 MONGODB_URI="mongodb+srv://j25759298_db_user:y6Kf7b33aj3jaadx@cluster0.pgyhwyx.mongodb.net"
@@ -12,7 +11,6 @@ try:
     client = MongoClient(MONGODB_URI)
     db = client[MONGODB_USERNAME]
     
-    # Crear índices únicos para asegurar la consistencia de los datos
     db.alumnos.create_index("matricula", unique=True)
     db.maestros.create_index("num_empleado", unique=True)
     db.materias.create_index("codigo_materia", unique=True)
